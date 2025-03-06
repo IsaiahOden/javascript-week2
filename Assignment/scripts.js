4694acea02ec001ff00af3c7888a8a713d5294dd
FJOden <isaiahdukeoden@gmail.com>
2025-03-06 09:48:19 -0600

    Update scripts.js

--- Assignment/scripts.js
+++ Assignment/scripts.js
@@ -1,17 +1,19 @@
 // some code goes here.
 document.write("HERE'S JAVA")
 var nameInput;
+function alertuser(){
+    alert(`Thank you for signing in ${nameInput}`);
+};
 
 window.onload = function() {
     if (window.location.pathname === "/Assignment/index.html") {
         nameInput = prompt(`Congratulations and Welcome to Fight Club!!! \n \nPlease Enter Your Name`)
     }
-        if (typeof nameInput === "string") {
-            alert(`Thank You For Signing In ${nameInput}!`)
-
-        } else {
-            nameInput = prompt("Dude/Dudette your name has to be letters")
-        }
+        while (nameInput === "") {
+            nameInput = prompt('Please enter something');
+        } 
+        alertuser();
+        
 
     
 }
